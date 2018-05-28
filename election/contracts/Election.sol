0aad2abcaf41f1f786528d8458f6df95a1a252eb
Sotrek <sotrek@gmail.com>
2018-05-28 15:45:26 +0100

    feat: listing candidates retrieved from contract

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -1,13 +1,29 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-	//Store candidate
-	//Read candidate
-	string public candidate;
-	//Constructor
-	// function Election () public { -- deprecated
-	constructor () public {
-		candidate = 'Candidate 1';
+	//Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
+	//Store Candidates
+	//Fetch Candidates
+	mapping(uint => Candidate) public candidates;
+
+	//Store Candidates count
+	uint public candidatesCount;
+
+	constructor () public {
+		addCandidate('Candidate 1');
+		addCandidate('Candidate 2');
+	}
+
+	//add candidate to mapping
+
+	function addCandidate(string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
